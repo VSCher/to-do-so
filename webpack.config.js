c741a3b554c3ef82b81d284070014f8a2778a8c8
vscpr-pc\vscpr <vscher@bk.ru>
2017-03-22 10:21:46 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        index: './index.jsx',
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: 'to-do-so/dist',
+        filename: '[name].bundle.js',
+        chunkFilename: '[id].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!sass-loader',
+            },
+            {
+                test: /\.jpg$/,
+                loader: 'file?name=[path][name].[ext]',
+            }
+        ],
+    },
+    devtool: 'eval',
+    plugins: [],
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+            }
+        }
+    },
+    externals: {
+        'cheerio': 'window',
+        'react/addons': true, // important!!
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+    }
+};
